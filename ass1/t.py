f3d64cadd715748152a97eb57f8f440431465be2
losmobilos3 <losmobilos3@gmail.com>
2025-09-01 13:35:13 +0200

    a

---
+++ ass1/t.py
@@ -0,0 +1,16 @@
+
+import irsim
+
+env = irsim.make('conf.yaml', projection="2d") # initialize the environment with the configuration file
+
+for i in range(300): # run the simulation for 300 steps
+    if i == 60:
+        print("hej")
+        env.robot_list[0].set_goal([1, 1, 0])
+
+    env.step()  # update the environment
+    env.render() # render the environment
+
+    if env.done(): break # check if the simulation is done
+        
+env.end() # close the environment
