42d8b1dda9eeb9449188120559ff300a30516b3a
Asser <asser29@gmail.com>
2025-09-02 11:48:54 +0200

    More work on circle_basic / regulator

--- ass1/t.py
+++ ass1/t.py
@@ -5,9 +5,9 @@ env = irsim.make('conf.yaml', projection="2d") # initialize the environment with
 env.load_behavior("custom_behaviour")
 
 for i in range(300): # run the simulation for 300 steps
-    if i == 60:
-        print("hej")
-        env.robot_list[0].set_goal([1, 1, 0])
+    # if i == 60:
+    #     print("hej")
+    #     env.robot_list[0].set_goal([1, 1, 0])
 
     env.step()  # update the environment
     env.render() # render the environment
