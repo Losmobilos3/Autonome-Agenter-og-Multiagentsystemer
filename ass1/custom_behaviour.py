1b255ede62fa196a23298c269d511c3d4d15b952
losmobilos3 <losmobilos3@gmail.com>
2025-09-08 20:51:50 +0200

    Implemented some attempt at RL

--- ass1/custom_behaviour.py
+++ ass1/custom_behaviour.py
@@ -4,7 +4,7 @@ from irsim.lib import register_behavior
 from irsim.util.util import WrapToPi, relative_position
 
 @register_behavior("diff", "RL_Circle")
-def beh_diff_RL_circle(ego_object, radius=0.5, external_objects = None, **kwargs):
+def beh_diff_RL_circle(ego_object, radius=2, external_objects = None, **kwargs):
     """
     Utilized reinforcement learning to get the object to move in a circle, around the goal point
 
@@ -17,28 +17,24 @@ def beh_diff_RL_circle(ego_object, radius=0.5, external_objects = None, **kwargs
         np.array: Velocity [linear, angular] (2x1).
     """
     
-    learner = Learner()
 
-    alpha = 0.1   # learning rate
-    gamma = 0.9   # discount factor
+    learner = Learner()
+    distFromCenter, _ = relative_position(ego_object.goal, ego_object.state)
+    if distFromCenter < radius - 0.05:
+       currState = 0
+    elif distFromCenter > radius + 0.05:
+       currState = 0
+    else:
+       currState = 1
+
+    action = learner.update_Q(currState, distFromCenter, radius)
 
     # TODO: implement q-learning (TD-learning + exploration)
     # States: i goldylock zone, too close to center, too far from center
 
-    
-
-    state = ego_object.state
-    center = ego_object.goal
-
-    # Pick a random angle (-5 / +5) (velocity is constant)
-
-    # Calculate if the robot is getting futher away from the desired radius.
-    # If yes: punish
-    # If no: Good!
-
     # Profit!
     linear = 1 # Scale linear velocity
-    angular = 1 # Change in angular velocity pr. frame
+    angular = -1 if action == 0 else 1 if action == 2 else 0
 
 
     # Return velocity and angle
@@ -49,11 +45,49 @@ def judge_angle(angle):
 
 
 class Learner(object):
+  # INITIALIZE LEARNER
   def __new__(cls):
     if not hasattr(cls, 'instance'):
       cls.instance = super(Learner, cls).__new__(cls)
-      cls.Q = np.ndarray([2, 3]) # 3 strates, 3 actions (right, left, straight)
-      
+      cls.Q = np.random.rand(2, 3) # 3 states, 3 actions (right, left, straight)
+      cls.Q[1, :] = np.zeros(3)
+      cls.alpha = 0.1   # learning rate
+      cls.gamma = 0.9   # discount factor
+
+      # Last values
+      cls.lastState = None
+      cls.lastAction = None
+      cls.lastR = None
     return cls.instance
+  
+  # DEFINE LEARNING/DECISION FUNCTION
+  # TODO: Maybe split into two functions
+  def update_Q(self, currState, distance, radius=0.5):
+    # Call with first currState different from 1
+    if (self.lastState is None) or (self.lastAction is None) or (self.lastR is None):
+        print("SE HER - First call, initializing values")
+        self.lastState = currState
+        action = np.argmax(self.Q[currState, :])
+        self.lastAction = action
+        self.lastR = -abs(distance - radius)  # Reward is higher the closer we are to the desired radius
+        return action
+    else:
+        # Update Q-table based on previous experience
+        print("SE HER - Updating Q-table:", self.lastState, self.lastAction, self.lastR)
+        self.Q[self.lastState, self.lastAction] = self.Q[self.lastState, self.lastAction] + self.alpha * (self.lastR + self.gamma * np.max(self.Q[currState, :]) - self.Q[self.lastState, self.lastAction])
+        
+        # If in terminal state, reset
+        if currState == 1:
+            self.lastState = None
+            self.lastAction = None
+            self.lastR = None
+            return 1
+            
+        # Update for next iteration
+        self.lastState = currState
+        action = np.argmax(self.Q[currState, :])
+        self.lastAction = action
+        self.lastR = -abs(distance - radius)
+        return action
 
   
--- ass1/t.py
+++ ass1/t.py
@@ -1,10 +1,9 @@
-#import custom_behaviour
 import irsim
 
 env = irsim.make('conf.yaml', projection="2d") # initialize the environment with the configuration file
 env.load_behavior("custom_behaviour")
 
-for i in range(300): # run the simulation for 300 steps
+for i in range(3000): # run the simulation for 300 steps
     # if i == 60:
     #     print("hej")
     #     env.robot_list[0].set_goal([1, 1, 0])
