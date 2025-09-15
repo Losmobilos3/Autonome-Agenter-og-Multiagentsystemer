2f17854a1beb5f192db6e1dd91255dd7bc2f0903
losmobilos3 <losmobilos3@gmail.com>
2025-09-15 15:20:22 +0200

    Added some comments

--- ass1/t.py
+++ ass1/t.py
@@ -18,6 +18,7 @@ for e in range(episodes):
         agent = Agent(robot)
         agent.reset_metrics()
 
+    # Run the episode
     for i in range(100 + e * 50): # run the simulation for 300 steps
         env.step()  # update the environment
         record_metrics(env.robot_list)
@@ -28,18 +29,18 @@ for e in range(episodes):
     # Print stats
     first_crash = None
     num_crashes = 0
-    cumulated_mse = 0
+    cumulated_mse_per_frame = 0
+
     for robot in env.robot_list:
         agent = Agent(robot)
         mse = agent.read_mse()
-        cumulated_mse += mse if mse is not None else 0
-        print(f"Episode {e}, Step {i}, Robot {robot.id}, MSE: {(mse / i):.4f}")
+        cumulated_mse_per_frame += mse / i if mse is not None else 0
+        mse_per_frame = mse / i
+        print(f"Episode {e}, Step {i}, Robot {robot.id}, MSE: {(mse_per_frame):.4f}")
         num_crashes += int(agent.read_crashed())
         
     first_crash = min([agent.read_crash_time() for robot in env.robot_list if (agent := Agent(robot)).read_crashed()] + [float('inf')]) # inf added in case of no crashes
-
+    cumulated_mse_per_frame /= len(env.robot_list)
     print(f"Episode {e} ended after {i} steps with {num_crashes} crashes. First crash at {first_crash} seconds.")
-    print(f"Average MSE for episode {e}: {(cumulated_mse / len(env.robot_list) / i):.4f}")
+    print(f"Average MSE for episode {e}: {(cumulated_mse_per_frame):.4f}")
     env.end()
-
-env.end() # close the environment
