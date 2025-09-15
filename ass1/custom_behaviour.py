873fcd368b3d994aea9cb00ebd1bdd39f0b4b188
losmobilos3 <losmobilos3@gmail.com>
2025-09-15 13:59:33 +0200

    Added a framework for having metrics.
    Co-authored-by: Asser Kaas Andersen <AsserAKA@users.noreply.github.com>
    Co-authored-by: Andreas-Bugge <Andreas-Bugge@users.noreply.github.com>
    Co-authored-by: Alexander Anker Christensen <A-CHRI@users.noreply.github.com>

--- ass1/custom_behaviour.py
+++ ass1/custom_behaviour.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time 
 from irsim.lib import register_behavior
 from irsim.util.util import WrapToPi, relative_position
 
@@ -17,10 +17,10 @@ def beh_diff_RL_circle(ego_object, radius=2, external_objects = None, **kwargs):
         np.array: Velocity [linear, angular] (2x1).
     """
     
-    learner = Learner(ego_object.id)
-    currState, distFromCenter = get_state(ego_object, radius, ego_object.goal)
+    agent = Agent(ego_object, radius)
+    currState, distFromCenter = get_state(ego_object, radius)
 
-    action = learner.update_Q(currState, distFromCenter, radius)
+    action = agent.learner.update_Q(currState, distFromCenter, radius)
 
     # TODO: implement q-learning (TD-learning + exploration)
     # States: i goldylock zone, too close to center, too far from center
@@ -29,13 +29,13 @@ def beh_diff_RL_circle(ego_object, radius=2, external_objects = None, **kwargs):
     linear = 1 # Scale linear velocity
     angular = -1 if action == 0 else 1 if action == 2 else 0
 
+    agent.update_metrics()
 
     # Return velocity and angle
     return np.array([[linear], [angular]])
 
-def get_state(ego_object, radius, goal):
-    dist_from_center, angle_to_center = relative_position(goal, ego_object.state)
-    print("Angle: ", angle_to_center)
+def get_state(ego_object, radius):
+    dist_from_center, angle_to_center = relative_position(ego_object.goal, ego_object.state)
     # Discretize distance (3 zones)
     if dist_from_center < radius - 0.1:
         dist_state = 0  # too close
@@ -52,29 +52,68 @@ def get_state(ego_object, radius, goal):
     if heading_error < -np.pi/8:
         heading_state = 0
     elif heading_error < np.pi/8:
-        heading_state = 1
+        heading_state = 1 # Good angle
     else:
         heading_state = 2
         
     return dist_state * 3 + heading_state, dist_from_center  # Combined state (0-8)
 
 
-class Learner(object):
-  # INITIALIZE LEARNER
-  def __new__(cls, id):
-    if not hasattr(cls, 'instances'):
-      cls.instances = {}
-    if id not in cls.instances:
-      cls.instances[id] = super(Learner, cls).__new__(cls)
-      cls.Q = np.random.rand(9, 3) # 9 states, 3 actions (right, left, straight)
-      cls.alpha = 0.1   # learning rate
-      cls.gamma = 0.4   # discount factor
+class Agent(object):
+    def __new__(cls, ego_object, desired_radius=2):
+        if not hasattr(cls, 'instances'):
+            cls.instances = {}
+        if ego_object.id not in cls.instances:
+            agent = super(Agent, cls).__new__(cls)
+            agent.learner = Learner()
+            agent.metrics = {}
+            agent.ego_object = ego_object
+            agent.r = desired_radius
+            cls.instances[ego_object.id] = agent
+        return cls.instances[ego_object.id]
+    
+    def update_metrics(self):
+        self._update_mse()
+        self._update_time_crashed()
+
+    def _update_mse(self):
+        if 'mse_stats' not in self.metrics:
+            self.metrics['mse_stats'] = {"cumulated_error": 0, "count": 0}
+        d, _ = relative_position(self.ego_object.goal, self.ego_object.state)
+        self.metrics['mse_stats']["cumulated_error"] += (self.r-d)**2
+        self.metrics['mse_stats']["count"] += 1
+
+    def read_mse(self):
+        if 'mse_stats' not in self.metrics:
+            print("ERROR: NO MSE AVAILABLE!")
+            return None
+        return self.metrics['mse_stats']["cumulated_error"] / self.metrics['mse_stats']["count"]
+    
+    def _update_time_crashed(self):
+        if 'crash_stats' not in self.metrics:
+            self.metrics['crash_stats'] = {"crashed_time": 0, "total_time": time.time()}
+        if self.ego_object.collision_flag:
+            self.metrics['crashed_time'] += time.time() - self.metrics['crash_stats']['total_time'] # TODO: set to time
+
+    def read_crashed(self):
+        return self.ego_object.collision_flag
+    
+    def read_crash_time(self):
+        if 'crash_stats' not in self.metrics:
+            print("ERROR: NO CRASH STATS AVAILABLE!")
+            return None
+        return self.metrics['crash_stats']['crashed_time']
+
+class Learner:
+  def __init__(self):
+      self.Q = np.random.rand(9, 3) # 9 states, 3 actions (right, left, straight)
+      self.alpha = 0.1   # learning rate
+      self.gamma = 0.4   # discount factor
 
       # Last values
-      cls.lastState = None
-      cls.lastAction = None
-      cls.lastR = None
-    return cls.instances[id]
+      self.lastState = None
+      self.lastAction = None
+      self.lastR = None
   
   # DEFINE LEARNING/DECISION FUNCTION
   def update_Q(self, currState, distance, radius=0.5):
--- ass1/t.py
+++ ass1/t.py
@@ -1,4 +1,5 @@
 import irsim
+from custom_behaviour import Agent
 
 episodes = 10
 
@@ -6,14 +7,22 @@ for e in range(episodes):
     env = irsim.make('conf.yaml', projection="2d") # initialize the environment with the configuration file
     env.load_behavior("custom_behaviour")
     for i in range(100 + e * 50): # run the simulation for 300 steps
-        # if i == 60:
-        #     print("hej")
-        #     env.robot_list[0].set_goal([1, 1, 0])
-
         env.step()  # update the environment
         env.render() # render the environment
 
         if env.done(): break # check if the simulation is done
+
+    # Print stats
+    first_crash = None
+    num_crashes = 0
+    for robot in env.robot_list:
+        agent = Agent(robot)
+        mse = agent.read_mse()
+        print(f"Episode {e}, Step {i}, Robot {robot.id}, MSE: {mse:.4f}")
+        num_crashes += int(agent.read_crashed())
+        first_crash = agent.read_crash_time() if first_crash is None else first_crash
+
+    print(f"Episode {e} ended after {i} steps with {num_crashes} crashes. First crash at {first_crash} seconds.")
     env.end()
 
 env.end() # close the environment
