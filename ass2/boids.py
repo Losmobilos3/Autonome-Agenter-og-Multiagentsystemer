b9bf79765741d14989024a16ef2042da34d37e76
losmobilos3 <losmobilos3@gmail.com>
2025-10-27 13:15:31 +0100

    .

--- ass2/boids.py
+++ ass2/boids.py
@@ -1,9 +1,12 @@
 import irsim
+import numpy as np
 
 env = irsim.make()
-env.load_behavior("custom_behaviour")
+env.load_behavior("follower_behaviour")
+# for robot in env.robot_list:
+    # robot.set_velocity = np.random.rand(2)
 
-for _i in range(1000):
+for _i in range(10000):
     env.step()
     env.render(figure_kwargs={"dpi": 100})
 env.end()
--- ass2/follower_behaviour.py
+++ ass2/follower_behaviour.py
@@ -1,7 +1,8 @@
 import numpy as np
 from irsim.lib import register_behavior
+import irsim
 
-@register_behavior("diff", "follower")
+@register_behavior("omni", "follower")
 def beh_follower(ego_object, external_objects=None, **kwargs):
     """
     Employs the boid behavior pattern, following a specified leader
@@ -13,6 +14,7 @@ def beh_follower(ego_object, external_objects=None, **kwargs):
     Returns:
         np.array: Velocity [linear, angular] (2x1).
     """
+
     if external_objects is None:
         external_objects = []
 
@@ -21,12 +23,19 @@ def beh_follower(ego_object, external_objects=None, **kwargs):
     S = kwargs["S"]
     A = kwargs["A"]
     C = kwargs["C"]
+    leader_prio = kwargs["leader_prio"]
 
+    leaders = [robot for robot in external_objects if distance_to_object(ego_object.position, robot.position) < radius and robot.name == "leader"]
     neighbors = [robot for robot in external_objects if distance_to_object(ego_object.position, robot.position) < radius]
     close_neighbors = [robot for robot in external_objects if distance_to_object(ego_object.position, robot.position) < protect_radius]
 
+    dv_stay_on_screen = stay_on_screen(ego_object)
+
     if len(neighbors) == 0 or np.linalg.norm(ego_object.velocity_xy) == 0:
-        return np.array([[1], [0]])
+        if np.linalg.norm(ego_object.velocity_xy) > 0:
+            return ego_object.velocity_xy + dv_stay_on_screen
+        else:
+            return np.random.rand(2, 1)
 
     # Separation: Each agent should steer to avoid crowding amongst other agents
     dv_sep = compute_separation(ego_object, close_neighbors)
@@ -37,16 +46,25 @@ def beh_follower(ego_object, external_objects=None, **kwargs):
     # Cohesion: Each agent should steer towards the leader
     dv_cohesion = compute_cohesion(ego_object, neighbors)
 
-    dv_stay_on_screen = stay_on_screen(ego_object)
+    dv_leader = np.zeros((2, 1))
+    if (len(leaders) > 0):
+        dv_sep_leader = compute_separation(ego_object, leaders)
+
+        dv_align_leader = compute_alignment(ego_object, leaders)
+
+        dv_cohesion_leader = compute_cohesion(ego_object, leaders)
+
+        dv_leader = S * dv_sep_leader + A * dv_align_leader + C * dv_cohesion_leader
 
     # Compute new heading
-    dv_total = S * dv_sep + A * dv_align + C * dv_cohesion
-    linear = np.linalg.norm(dv_total)
-    angular = ego_object.velocity_xy.T @ dv_total / (np.linalg.norm(ego_object.velocity_xy) * np.linalg.norm(dv_total))
+    dv_total = (1 - leader_prio) * (S * dv_sep + A * dv_align + C * dv_cohesion) + 10* dv_stay_on_screen + leader_prio * dv_leader
+    # linear = np.linalg.norm(dv_total)
+    # angular = ego_object.velocity_xy.T @ dv_total / (np.linalg.norm(ego_object.velocity_xy) * np.linalg.norm(dv_total))
 
     # Return velocity and angle
     # TODO: Skal vi droppe linear speed aspektet?
-    return np.array([[linear], [angular.item()]])
+    #return np.array([[linear], [angular.item()]])
+    return dv_total
 
 def distance_to_object(obj_pos_a, obj_pos_b):
     return np.linalg.norm(obj_pos_a - obj_pos_b)
@@ -59,15 +77,24 @@ def compute_separation(ego_object, neighbors):
     return dv
 
 def compute_alignment(ego_object, neighbors):
-    avg_vel = np.average([n.velocity_xy for n in neighbors], axis=0)
-    dv = avg_vel - ego_object.velocity_xy
+    # Align with leader and not others
+    avg_v = np.mean([n.velocity_xy for n in neighbors])
+    dv = avg_v
     return dv
 
 def compute_cohesion(ego_object, neighbors):
     avg_position = np.average([n.position for n in neighbors], axis=0)
-    dv = avg_position - ego_object.position 
+    dv = avg_position - ego_object.position
     return dv
 
 def stay_on_screen(ego_object):
-    # TODO
-    return np.array([0, 0])
+    dv = np.zeros((2, 1))
+    if ego_object.position[0] < 5:
+        dv += np.eye(2)[:, [0]]
+    elif ego_object.position[0] > 45:
+        dv -= np.eye(2)[:, [0]]
+    if ego_object.position[1] < 5:
+        dv += np.eye(2)[:, [1]]
+    elif ego_object.position[1] > 45:
+        dv -= np.eye(2)[:, [1]]
+    return dv
