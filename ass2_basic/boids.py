514cfa3844dc16eca7e823b310300f65732f9408
achri <au729146@uni.au.dk>
2025-10-27 15:01:25 +0100

    It works wuhuu

--- ass2_basic/boids.py
+++ ass2_basic/boids.py
@@ -3,7 +3,7 @@ import irsim
 env = irsim.make()
 env.load_behavior("custom_behaviour")
 
-for _i in range(1000):
+for _i in range(2000):
     env.step()
     env.render(figure_kwargs={"dpi": 100})
 env.end()
--- ass2_basic/custom_behaviour.py
+++ ass2_basic/custom_behaviour.py
@@ -1,7 +1,7 @@
 import numpy as np
 from irsim.lib import register_behavior
 
-@register_behavior("diff", "follower")
+@register_behavior("omni", "follower")
 def beh_follower(ego_object, external_objects=None, **kwargs):
     """
     Employs the boid behavior pattern, following a specified leader
@@ -17,42 +17,78 @@ def beh_follower(ego_object, external_objects=None, **kwargs):
         external_objects = []
 
     radius = kwargs["radius"]
+    separation_strength = kwargs["separation_strength"]
+    alignment_strength = kwargs["alignment_strength"]
+    cohesion_strength = kwargs["cohesion_strength"]
+    separation_radius = kwargs["separation_radius"]
+    turn_factor = kwargs["turn_factor"]
 
     neighbors = [robot for robot in external_objects if distance_to_object(ego_object.position, robot.position) < radius]
     leader = next((robot for robot in neighbors if robot.name == "leader"), None)
 
     # Separation: Each agent should steer to avoid crowding amongst other agents
-    separation = compute_separation(ego_object, neighbors)
+    separation = compute_separation(ego_object, neighbors, separation_radius)
 
     # Alignment: Each agent should steer towards the heading of the leader
     alignment = compute_alignment(ego_object, leader)
 
     # Cohesion: Each agent should steer towards the leader
-    cohesion = compute_cohesion(leader)
+    cohesion = compute_cohesion(ego_object, leader)
 
-    # Compute new heading
-    angular = separation + alignment + cohesion
+    # Compute new vector
+    steering = separation_strength * separation + alignment_strength * alignment + cohesion_strength * cohesion
 
-    # Return velocity and angle
-    return np.array([[1], [angular]])
+    # Add wall avoidance
+    steering += turn_factor * avoid_wall(ego_object)
+
+    return steering
 
 def distance_to_object(obj_pos_a, obj_pos_b):
     return np.linalg.norm(obj_pos_a - obj_pos_b)
 
-def compute_separation(ego_object, neighbors):
-    angular = 0
-    for neighbor in neighbors:
-        angular -= ego_object.heading - neighbor.heading
-    return angular
+def compute_separation(ego_object, neighbors, radius):
+    closest_neighbors = [neighbor for neighbor in neighbors if distance_to_object(ego_object.position, neighbor.position) < radius]
+
+    # No change if no neighbor is too close
+    if len(closest_neighbors) == 0:
+        return ego_object.velocity
+
+    steering = 0
+    for neighbor in closest_neighbors:
+        steering += ego_object.position - neighbor.position
+
+    return steering / len(closest_neighbors)
 
 def compute_alignment(ego_object, leader):
     if not leader:
-        return 0
+        return ego_object.velocity
 
-    return ego_object.heading - leader.heading
+    return leader.velocity
 
-def compute_cohesion(leader):
+def compute_cohesion(ego_object, leader):
     if not leader:
-        return 0
+        return ego_object.velocity
 
-    return leader.heading
+    steering = leader.position - ego_object.position
+    return steering
+
+def avoid_wall(ego_object):
+    steering = 0
+
+    # Avoid left wall
+    if ego_object.position[0] < 5:
+        steering += np.array([[1], [0]])
+
+    # Avoid right wall
+    if ego_object.position[0] > 45:
+        steering += np.array([[-1], [0]])
+
+    # Avoid top wall
+    if ego_object.position[1] > 45:
+        steering += np.array([[0], [-1]])
+
+    # Avoid right wall
+    if ego_object.position[1] < 5:
+        steering += np.array([[0], [1]])
+
+    return steering
