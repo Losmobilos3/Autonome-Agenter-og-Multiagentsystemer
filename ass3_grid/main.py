5eaf21dfb43d8bef139b9e297b6eab37f3baa25e
Daniel Østerballe <Losmobilos3@gmail.com>
2026-01-03 15:54:20 +0100

    fix

--- ass3_grid/main.py
+++ ass3_grid/main.py
@@ -5,7 +5,7 @@ from matplotlib import animation
 no_agent_runs = [1, 2, 3, 4, 5]
 
 with open("data.txt", "w") as f:
-    f.write("no_agents, performance, fruits_1, fruits_2")
+    f.write("no_agents, performance, fruits_1, fruits_2\n")
     for no_agents in no_agent_runs:
         print(f"START TRAINING RUN {no_agents}")
 
@@ -16,7 +16,7 @@ with open("data.txt", "w") as f:
             height = 20,
         )
 
-        sim.run_episodes(no_episodes=100, max_steps_per_episode=300)
+        sim.run_episodes(no_episodes=1, max_steps_per_episode=300)
 
         sim.init_env()
 
@@ -40,10 +40,10 @@ with open("data.txt", "w") as f:
 
         f.write(f"{no_agents}, {performance}, {sim.total_fruits_collected[0]}, {sim.total_fruits_collected[1]}\n")
 
-    try:
-        ani.save(f'MARL_simulation_animation_{no_agents}.mp4', writer='ffmpeg', fps=24)
-        print("Animation saved successfully")
-    except Exception as e:
-        print(f"Error saving animation: {e}")
+        try:
+            ani.save(f'MARL_simulation_animation_{no_agents}.mp4', writer='ffmpeg', fps=24)
+            print("Animation saved successfully")
+        except Exception as e:
+            print(f"Error saving animation: {e}")
 
         plt.close(sim.fig)
